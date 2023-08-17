72f5593be228b5df58ccc2545985d502fa033641
elba aban <elbajonas16@gmail.com>
2023-08-17 20:07:07 -0300

    banner de marvel

---
+++ banerMarvel/marvel.js
@@ -0,0 +1,11 @@
+
+  const paragraph = document.getElementById("paragraph");
+  const words = paragraph.textContent.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 2) {
+      words[i] = `<span class="highlight">${words[i]}</span>`;
+    }
+  }
+
+  paragraph.innerHTML = words.join(" ");
